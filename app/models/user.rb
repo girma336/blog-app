8723a92f862dea008d0c567abd64ca56dfa88e0f
girma <girmatarekegn.gi@gmail.com>
2023-03-09 18:43:36 +0300

    Add all give validation for model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ApplicationRecord
   has_many :posts
   has_many :comments
 
+  validates :name, presence: true
+  validates :posts_counter, numericality: { only_inter: true, greater_than_or_equal_to: 0 }
   def three_most_recent_posts
     posts.order(created_at: :desc).limit(3)
   end
