9bd4dfaed356eb4ef6eda83aec3b36a9539d9225
girma <girmatarekegn.gi@gmail.com>
2023-03-25 20:49:28 +0300

    Add documentation

---
+++ app/controllers/api/comments_controller.rb
@@ -0,0 +1,21 @@
+class Api::CommentsController < ApplicationController
+  def index
+    @comments = Comment.all.where(author_id: params[:user_id], post_id:params[:post_id])
+    render json: @comments
+  end
+
+  def create
+    @comment = Comment.new(comment_params)
+      if @comment.save
+        render json: @comment, status: :created, location: @comment
+      else
+        render json: @comment.errors, status: :unprocessable_entity
+      end
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:title, :text)
+  end
+end
