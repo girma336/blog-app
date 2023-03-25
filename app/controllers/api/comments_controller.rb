019ef176a7ed9735d531f1b1425663892a0f9e26
girma <girmatarekegn.gi@gmail.com>
2023-03-25 23:17:28 +0300

    fixed linter error

--- app/controllers/api/comments_controller.rb
+++ app/controllers/api/comments_controller.rb
@@ -1,15 +1,15 @@
 class Api::CommentsController < ApplicationController
   def index
-    @comments = Comment.all.where(author_id: params[:user_id], post_id:params[:post_id])
+    @comments = Comment.all.where(author_id: params[:user_id], post_id: params[:post_id])
   end
 
   def create
     @comment = Comment.new(comment_params)
-      if @comment.save
-        render json: @comment, status: :created, location: @comment
-      else
-        render json: @comment.errors, status: :unprocessable_entity
-      end
+    if @comment.save
+      render json: @comment, status: :created, location: @comment
+    else
+      render json: @comment.errors, status: :unprocessable_entity
+    end
   end
 
   private
